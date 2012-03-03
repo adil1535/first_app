4424fd47bc34b913bf9616e8339b114958da1d0f
Adil <itsaboutcode@amna-Inspiron-N4050.(none)>
2012-03-04 00:04:32 +0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '3f17b2ae78686b8ef709048656374d06e99a9b2faf75a8a9f93feb78a2454deb3a3347a91f359f921c7b817807ddaeba903892d3aa315df8a69ea710f6b6e127'
